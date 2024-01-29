96527fe58f014bf3ebf15946b97cc03ac1b0106c
miguel-mayo <71817857+miguel-mayo@users.noreply.github.com>
2024-01-29 20:24:16 +0100

    Update mayo_cloud_firestore.dart

--- lib/mayo_cloud_firestore.dart
+++ lib/mayo_cloud_firestore.dart
@@ -2,4 +2,5 @@ library mayo_cloud_firestore;
 
 export 'src/converters/reference_converter.dart';
 export 'src/converters/timestamp_converter.dart';
+export 'src/converters/timestamp_map_converter.dart';
 export 'src/firestore_data_provider.dart';
